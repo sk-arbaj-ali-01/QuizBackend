9f953ac00c716578e8c64826230a78aee3006cdb
SK ARBAJ ALI <skarbajali@protonmail.com>
2026-07-26 21:48:15 +0530

    Refactor: add name based search for relate-teacher-to-student api. also add pagination query to this api. (#10)

---
+++ src/Quiz.DL/Parameters/RelatedTeachersParameter.cs
@@ -0,0 +1,5 @@
+﻿namespace Quiz.DL.Parameters;
+public class RelatedTeachersParameter : BaseParameter
+{
+    public string SearchQuery { get; set; } = string.Empty;
+}
--- src/Quiz.DL/Repositories/UserRepository.cs
+++ src/Quiz.DL/Repositories/UserRepository.cs
@@ -3,10 +3,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Quiz.DL.Abstractions;
 using Quiz.DL.Entities;
+using Quiz.DL.Parameters;
 using Quiz.DL.Service;
 using Quiz.DL.SQLQueries;
 using Quiz.Shared.DTO.User.Response;
+using Quiz.Shared.Helpers;
 using Quiz.Shared.Models;
+using System.Text;
+using System.Data;
 
 namespace Quiz.DL.Repositories;
 public class UserRepository(
@@ -49,13 +53,42 @@ public class UserRepository(
         return response;
     }
 
-    public async Task<PagedRecordModel<UserTeacherResponseDto>> GetTeachersData()
+    public async Task<PagedRecordModel<UserTeacherResponseDto>> GetTeachersData( 
+        RelatedTeachersParameter parameter,
+        Guid userId)
     {
+        StringBuilder sqlBuilder = new(UserSqlQueries.GetTeachersData);
+        DynamicParameters dynamicParameters = new DynamicParameters();
+
+        dynamicParameters.Add("StudentId", userId, DbType.Guid);
+
+        if (Utility.IsValidString(parameter.SearchQuery))
+        {
+            sqlBuilder.Append(
+                @$" AND U.full_name LIKE @{nameof(parameter.SearchQuery)} ");
+            dynamicParameters.Add(nameof(parameter.SearchQuery), $"%{parameter.SearchQuery}%");
+        }
+
+        if(parameter.PerPage > 0 && parameter.Page > 0)
+        {
+            sqlBuilder.Append(
+                @$" ORDER BY U.user_id
+                    LIMIT {parameter.PerPage}
+                    OFFSET {parameter.PerPage * (parameter.Page - 1)}
+            ");
+        }
+        else
+        {
+            sqlBuilder.Append(@" ORDER BY U.user_id ");
+        }
+
         IEnumerable<UserTeacherResponseDto> results =
             await DbOperation(async connection =>
                 await connection.QueryAsync<UserTeacherResponseDto>(
-                    UserSqlQueries.GetTeachersData)
-                );
+                    sqlBuilder.ToString(),
+                    dynamicParameters
+                )
+            );
 
         return new PagedRecordModel<UserTeacherResponseDto>
         {
